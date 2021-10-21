dc07b62b93b9e15e63df09271ef3171414a75f5f
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-10-21 16:10:22 +0200

    Quiz App functional and working

--- screens/Quiz/Quiz.js
+++ screens/Quiz/Quiz.js
@@ -1,17 +1,63 @@
 import React, { useState } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Modal, Animated } from 'react-native'
 import styles from './styles'
 import data from '../../data/QuizData';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { SafeAreaView, useSafeAreaFrame } from 'react-native-safe-area-context';
+import  MaterialCommunityIcons  from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 
 const Quiz = (props) => {
 
     const allQuestions = data;
-    const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0)
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+    const [currentOptionSelected, setCurrentOptionSelected] = useState(null);
+    const [correctOption, setCorrectOption] = useState(null);
+    const [isOptionsDisabled, setIsOptionsDisabled] = useState(false);
+    const [score, setScore] = useState(0)
+    const [showNextButton, setShowNextButton] = useState(false)
+    const [showScoreModal, setShowScoreModal] = useState(false)
 
+
+
+    const validateAnswer = (selectedOption) => {
+        let correct_option = allQuestions[currentQuestionIndex]['correct_option'];
+        setCurrentOptionSelected(selectedOption);
+        setCorrectOption(correct_option);
+        setIsOptionsDisabled(true);
+        if(selectedOption==correct_option){
+            // Set Score
+            setScore(score+1)
+        }
+        // Show Next Button
+        setShowNextButton(true)
+    }
+
+    const handleNext = () => {
+        if(currentQuestionIndex== allQuestions.length-1){
+            // Last Question
+            // Show Score Modal
+            setShowScoreModal(true)
+        }else{
+            setCurrentQuestionIndex(currentQuestionIndex+1);
+            setCurrentOptionSelected(null);
+            setCorrectOption(null);
+            setIsOptionsDisabled(false);
+            setShowNextButton(false);
+        }
+    }
+ const restartQuiz= () => {
+     setShowScoreModal(false);
+
+     setCurrentQuestionIndex(0);
+     setScore(0);
+     setCurrentOptionSelected(null);
+     setCorrectOption(null);
+     setIsOptionsDisabled(false);
+     setShowNextButton(false);
+
+
+ }
 
     const renderQuestion = () => {
         return (
@@ -39,15 +85,74 @@ const Quiz = (props) => {
         return (
             <View>
                 {
-                    allQuestions[currentQuestionIndex]?.options.map(option => (
-                <TouchableOpacity>
-                <Text>{option}</Text>
+                allQuestions[currentQuestionIndex]?.options.map(option => (
+                <TouchableOpacity
+                onPress={()=> validateAnswer(option)}
+                disabled={isOptionsDisabled}
+                key={option}
+                style={{
+                    
+                    borderWidth: 3,
+                    backgroundColor: '#098',
+                    height: 60, borderRadius: 20,
+                    flexDirection: 'row',
+                    alignItems: 'center', justifyContent: 'space-between',
+                    paddingHorizontal: 20,
+                    marginVertical: 10,
+                }}
+                >
+                 <Text style= {{ color: '#fff'}}>{option}</Text>
+                
+                   {
+                    option==correctOption ? (
+                        <View style={{
+                            width: 30, height: 30, borderRadius: 30/2,
+                            backgroundColor: '#fff',
+                            justifyContent: 'center', alignItems: 'center'
+                        }}>
+                            <MaterialCommunityIcons name="check" style={{
+                                color: '#000',
+                                fontSize: 20
+                            }} />
+                        </View>
+                    ): option == currentOptionSelected ? (
+                        <View style={{
+                            width: 30, height: 30, borderRadius: 30/2,
+                            backgroundColor: 'red',
+                            justifyContent: 'center', alignItems: 'center'
+                        }}>
+                            <MaterialCommunityIcons name="close" style={{
+                                color: '#fff',
+                                fontSize: 20
+                            }} />
+                        </View>
+                    ) : null
+
+                    }
                 </TouchableOpacity>
-                    )
-                    )
+                ))
                 }
             </View>
         )
+    }
+
+    const renderNextButton = () => {
+      if(showNextButton){
+          return(
+              <TouchableOpacity
+              onPress={handleNext} 
+              style={{
+                  marginTop: 20, width: '100%', padding: 20, borderRadius: 15,
+              }}>
+                  <Text style={{
+                      color: '#fff', backgroundColor: '#955',textAlign: 'center',
+                  }}>Next</Text>
+              </TouchableOpacity>
+          )
+
+      } else {
+          return null;
+      }
     }
 
     return (
@@ -57,6 +162,46 @@ const Quiz = (props) => {
         <View style={styles.quizContainer}>
             {renderQuestion()}
             {renderOptions()}
+            {renderNextButton()}
+            <Modal
+            animationType="slide"
+            transparent={true}
+            visible={showScoreModal}>
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#744',
+                }}>
+                    <View style={{
+                        backgroundColor: '#fff',
+                        width: '90%',
+                        borderRadius: 20,
+                        padding: 20,
+                        alignItems: 'center',
+                    }}>
+                        <Text style={{fontSize: 30, fontWeight: 'bold'}}>{ score> (allQuestions.length/2) ? 'Congratulations!' : 'Oops!' }</Text>
+                        <View style={{
+                            flexDirection: 'row',
+                            justifyContent: 'flex-start',
+                            alignItems: 'center',
+                            marginVertical: 20,
+                        }}>
+                            <Text>{score} </Text>
+                            <Text style={{
+                                    fontSize: 12, color: '#000'
+                                }}>/ { allQuestions.length }</Text>
+                        </View>
+                        <TouchableOpacity
+                        onPress={restartQuiz}>
+                            <Text>Retry Quiz</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+
+            </Modal>
+
             {/* <View style={styles.question}>
                 <Text style={styles.questionText}>Why?</Text>
             </View>
